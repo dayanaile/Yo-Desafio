1c4f37121981f6a6dee484bb694bf4f9dafd6ba4
Dayany Anaile <daya.anaile@gmail.com>
2021-06-14 17:21:00 -0400

    Resolve erro de ID

--- server.js
+++ server.js
@@ -9,20 +9,20 @@ const io = require("socket.io")(server, {
 	}
 })
 
-io.on("connection", (socket) =>{
-    socket.emit("me", socket.id)
+io.on("connection", (socket) => {
+	socket.emit("me", socket.id)
 
-    socket.on("disconnect", () => {
-        socket.broadcast.emit("callEnded")
-    })
+	socket.on("disconnect", () => {
+		socket.broadcast.emit("callEnded")
+	})
 
-    socket.on("callUser", (data) =>{
-        io.to(data.userToCall).emit("callUser", {signal: signalData, from: data.from, name: data.name})
-    })
+	socket.on("callUser", (data) => {
+		io.to(data.userToCall).emit("callUser", { signal: data.signalData, from: data.from, name: data.name })
+	})
 
-    socket.on("answerCall", (data) =>
-    io.to(data.to).emit("callAccepted"), data.signal )
-
+	socket.on("answerCall", (data) => {
+		io.to(data.to).emit("callAccepted", data.signal)
+	})
 })
 
 server.listen(5000, () => console.log("server is running on port 5000"))
