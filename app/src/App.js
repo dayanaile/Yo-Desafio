18a39acf3539ddcebee41e0fd26fc2e086fc0d32
Dayany Anaile <daya.anaile@gmail.com>
2021-06-14 20:01:07 -0400

    Atualiza Design

--- app/src/App.js
+++ app/src/App.js
@@ -95,53 +95,62 @@ function App() {
 
 	return (
 		<>
-		<h1 style={{ textAlign: "center", color: '#fff' }}>Yo Challenge</h1>
 		<div className="container">
-			<div className="video-container">
-				<div className="video">
-					{stream &&  <video playsInline muted ref={myVideo} autoPlay style={{ width: "300px" }} />}
-				</div>
-				<div className="video">
-					{callAccepted && !callEnded ?
-					<video playsInline ref={userVideo} autoPlay style={{ width: "300px"}} />:
-					null}
-				</div>
-			</div>
-			<div className="myId">
-				<TextField
-					id="filled-basic"
-					label="Name"
-					variant="filled"
-					value={name}
-					onChange={(e) => setName(e.target.value)}
-					style={{ marginBottom: "20px" }}
-				/>
-				<CopyToClipboard text={me} style={{ marginBottom: "2rem" }}>
-					<Button variant="contained" color="primary" startIcon={<AssignmentIcon fontSize="large" />}>
-						Copy ID
-					</Button>
-				</CopyToClipboard>
+      <div className="navbar">
+        <img
+          alt=""
+          src="/chat-de-video.png"
+          className="icon"
+        />{' '}
+        <h1>Yo Challenge</h1>
+      </div>
+      <div className="container__videoBox" >
+        <div className="video-container">
+          <div className="video">
+            {stream &&  <video className="videoBox" playsInline muted ref={myVideo} autoPlay/>}
+          </div>
+          <div className="video">
+            {callAccepted && !callEnded ?
+            <video className="videoBox" playsInline ref={userVideo} autoPlay/>:
+            null}
+          </div>
+        </div>
+        <div className="myId">
+          <TextField
+            id="filled-basic"
+            label="Name"
+            variant="filled"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            style={{ marginBottom: "20px" }}
+          />
+          <CopyToClipboard text={me} style={{ marginBottom: "2rem" }}>
+            <Button variant="contained" color="primary" startIcon={<AssignmentIcon fontSize="large" />}>
+              Copy ID
+            </Button>
+          </CopyToClipboard>
 
-				<TextField
-					id="filled-basic"
-					label="ID to call"
-					variant="filled"
-					value={idToCall}
-					onChange={(e) => setIdToCall(e.target.value)}
-				/>
-				<div className="call-button">
-					{callAccepted && !callEnded ? (
-						<Button variant="contained" color="secondary" onClick={leaveCall}>
-							End Call
-						</Button>
-					) : (
-						<IconButton color="primary" aria-label="call" onClick={() => callUser(idToCall)}>
-							<PhoneIcon fontSize="large" />
-						</IconButton>
-					)}
-					{idToCall}
-				</div>
-			</div>
+          <TextField
+            id="filled-basic"
+            label="ID to call"
+            variant="filled"
+            value={idToCall}
+            onChange={(e) => setIdToCall(e.target.value)}
+          />
+          <div className="call-button">
+            {callAccepted && !callEnded ? (
+              <Button variant="contained" color="secondary" onClick={leaveCall}>
+                End Call
+              </Button>
+            ) : (
+              <IconButton color="primary" aria-label="call" onClick={() => callUser(idToCall)}>
+                <PhoneIcon fontSize="large" />
+              </IconButton>
+            )}
+            {idToCall}
+          </div>
+        </div>
+      </div>
 			<div>
 				{receivingCall && !callAccepted ? (
 						<div className="caller">
